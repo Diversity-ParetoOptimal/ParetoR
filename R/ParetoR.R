45297fdd946a324cc21017d54ded39538809872b
Diversity-ParetoOptimal <qianqisong@gmail.com>
2024-02-08 15:26:02 -0500

    Update ParetoR.R

--- R/ParetoR.R
+++ R/ParetoR.R
@@ -1,7 +1,6 @@
 # Command Function (ParetoR) - Pareto-Optimization via Normal Boundary Intersection
-# Developer: Temporarily removed for blind review
-# Contact: Temporarily removed for blind review
-# Last Update: 02/12/2018
+# Developer: Q. Chelsea Song
+# Contact: qianqisong@gmail.com
 
 ###### Instruction ######
 
@@ -14,7 +13,7 @@
 #    a) prop (i.e., proportion of minority applicants)
 #    b) sr (i.e., selection ratio)
 #    c) d (i.e., subgroup difference)
-#    d) R (i.e., correlation matrix of predictor and criterions)
+#    d) R (i.e., correlation matrix of predictor and criteria)
 # 4. Run "out = ParetoR(prop, sr, R, d)" in R console or RStudio
 
 ## Input Description ##
@@ -31,37 +30,14 @@
 # 1) Pareto Optimal solution (i.e., AI ratio, Criterion Validity, Predictor Weights);
 # 2) Plots (i.e., AI ratio - Criterion Validity trade-off & predictor weights trade-off).
 
-###### Example Input ######
-
-# # DeCorte, Lievens & Sackett (2007) application example is used as example input below.
-#
-# # Proportion of minority applicants in full applicant pool
-# prop <<- 1/4
-#
-# # Selection ratio
-# sr <<- 0.10
-#
-# # Subgroup difference
-# d <<- c(1.00, 0.23, 0.09, 0.33)
-#
-# # Correlation matrix
-# # Format: Predictor_1, ..., Predictor_n, Criterion
-# R <<- matrix(c(1, .24, .00, .19, .30,
-#                .24, 1, .12, .16, .30,
-#                .00, .12, 1, .51, .18,
-#                .19, .16, .51, 1, .28,
-#                .30, .30, .18, .28, 1),
-#              (length(d)+1),(length(d)+1),byrow=T)
-#
-# ###### Additional Settings ######
-
 #' ParetoR
 #'
 #' Command function to run Pareto-Optimal algorithm.
-#' @param prop Proportion of minority applicants in full applicant pool
+#' @param prop Proportion of minority applicants in the applicant pool
 #' @param sr Selection ratio
 #' @param d Subgroup difference
 #' @param R Correlation matrix
+#' @non_negative_weights If TRUE, predictor weights will constrained to be non-negative
 #' @param graph If TRUE, plots will be generated for Pareto-optimal curve and predictor weights
 #' @return out Pareto-Optimal solution with criterion values (Criterion) and predictor weights (ParetoWeights)
 #' @examples
@@ -84,7 +60,7 @@
 #' out = ParetoR(prop, sr, d, R)
 #'
 #' @export
-ParetoR = function(prop, sr, d, R, Spac = 20, graph = TRUE, display_solution = TRUE){
+ParetoR = function(prop, sr, d, R, Spac = 20, non_negative_weights = TRUE, graph = TRUE, display_solution = TRUE){
 
   prop_ParetoR <<- prop
   sr_ParetoR <<- sr
@@ -99,9 +75,15 @@ ParetoR = function(prop, sr, d, R, Spac = 20, graph = TRUE, display_solution = T
   # Do not change
   Fnum 	= 2
   Xnum = length(d)
-  VLB = c(-(Xnum+1),rep(0,Xnum))
-  VUB = c((Xnum+1),rep(1,Xnum))
   X0 = c(1,rep(1/Xnum,Xnum))
+
+  if(non_negative_weights == TRUE){
+    VLB = c(-(Xnum+1),rep(0,Xnum))
+    VUB = c((Xnum+1),rep(1,Xnum))
+  }else if(non_negative_weights == FALSE){
+    VLB = c(-(Xnum+1),rep(-Inf,Xnum))
+    VUB = c((Xnum+1),rep(Inf,Xnum))
+  }
 
   ###### Find Pareto-Optimal Solution ######
 
